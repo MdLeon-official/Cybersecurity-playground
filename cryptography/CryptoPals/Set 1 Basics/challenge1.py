fb5fb8914f653256c5378644527d7493e894402c
MD Leon Islam <80670800+MdLeon-official@users.noreply.github.com>
2025-07-05 12:42:28 +0600

    Create challenge1.py

---
+++ cryptography/CryptoPals/Set 1 Basics/challenge1.py
@@ -0,0 +1,20 @@
+"""
+Convert hex to base64
+
+The string:
+49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+
+Should produce:
+SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+"""
+
+
+import base64
+
+hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+hex_to_text = bytes.fromhex(hex_string).decode()
+
+text_to_base64 = base64.b64encode(hex_to_text.encode())
+
+print(text_to_base64.decode())
